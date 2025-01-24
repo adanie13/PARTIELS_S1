3c423cb03a6cb8867d6e44b29f8170aa10efa00e
adanie13 <axeldanie059@gmail.com>
2025-01-24 10:33:48 +0000

    ajout de l'affichage des disponibilités

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,21 @@ int main() {
   int velosDispos = 20;
   int capacite = 20;
   int choix;
-
+  cin >> velosDispos;
+  cin >> capacite;
   cout << "****** Gestion d'une station Vélô ******" << endl << endl;
   cout << "----------------------------------------" << endl;
-  cout << "Vélôs disponibles : 20" << endl;
-  cout << "Points d'attache disponibles : 0" << endl;
+  cout << "Vélôs disponibles : " << velosDispos << endl;
+  cout << "Points d'attache disponibles : " << capacite - velosDispos << endl;
   cout << "----------------------------------------" << endl << endl;
   cout << "1. Emprunter un Vélô électrique" << endl;
   cout << "2. Restituer un Vélô électrique" << endl;
   cout << "3. Estimer le coût de la location" << endl;
   cout << "4. Quitter l'application" << endl << endl;
 
+  cin >> choix;
+  
+
+
   return 0;
+}

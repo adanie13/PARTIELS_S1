879dbcc7acae80acfaad7fca52f086d5dd619094
adanie13 <axeldanie059@gmail.com>
2025-01-24 09:54:13 +0000

    premier commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  const float coutHoraire = 4.0;
+  int velosDispos = 20;
+  int capacite = 20;
+  int choix;
+
+  cout << "****** Gestion d'une station Vélô ******" << endl << endl;
+  cout << "----------------------------------------" << endl;
+  cout << "Vélôs disponibles : 20" << endl;
+  cout << "Points d'attache disponibles : 0" << endl;
+  cout << "----------------------------------------" << endl << endl;
+  cout << "1. Emprunter un Vélô électrique" << endl;
+  cout << "2. Restituer un Vélô électrique" << endl;
+  cout << "3. Estimer le coût de la location" << endl;
+  cout << "4. Quitter l'application" << endl << endl;
+
+  return 0;
